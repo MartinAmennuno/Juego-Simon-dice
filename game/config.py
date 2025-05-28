8a4b97fd36a20cbd4293c34f818290971dbb7c84
Martin Amennuno <AmennunoMartin@gmail.com>
2025-05-28 13:18:25 -0300

    Add files via upload

---
+++ game/config.py
@@ -0,0 +1,38 @@
+# Colores
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+AMARILLO = (252, 255, 34)
+AZUL = (34, 15, 255)
+VERDE = (0, 255, 35)
+
+ROJO_OSCURO = (148, 3, 3)
+AMARILLO_OSCURO = (171, 168, 10)
+AZUL_OSCURO = (26, 19, 118)
+VERDE_OSCURO = (24, 149, 41)
+CELESTE = (3, 181, 229)
+
+# Dimenciones
+WIDTH = 1920
+HEIGHT = 1080
+CUADRADO_ROJO = (130, 130, 150, 150)
+CUADRADO_AMARILLO = (300, 130, 150, 150)
+CUADRADO_AZUL = (130, 300, 150, 150)
+CUADRADO_VERDE = (300, 300, 150, 150)
+
+# Fuentes
+TYPE_FONT = "Calibri"
+SIZE_FONT = 30
+TITLE = "SIMON"
+
+# Audios
+BEEP = "./game/sources/sounds/beep-02.mp3"
+WIN = "./game/sources/sounds/win.mp3"
+GAME_OVER = "./game/sources/sounds/game-over-arcade-6435.mp3"
+
+# Archivos
+PUNTAJE = "./game/sources/file/puntaje.txt"
+USUARIOS = "./game/sources/file/usuarios.txt"
+RANKING = "./game/sources/file/ranking.txt"
+RANKING_ORDEN = "./game/sources/file/ranking_ordenado.txt"
+ARCHIVO_REINICIO = "Jugador   0\n"
